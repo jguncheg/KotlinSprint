c010dcd5e385f9815173a5439ff679fff1f1047b
jguncheg <kelle@list.ru>
2023-09-14 20:33:33 +0300

    KS-1-2 добавлено количесвто работников

--- src/main/kotlin/lesson1_task2.kt
+++ src/main/kotlin/lesson1_task2.kt
@@ -2,7 +2,6 @@ fun main () {
     var orderCounter: Int = 75
     val messageToBuyer: String = "Спасибо за покупку, приходите к нам еще!"
 
-
     //val storeWorker: Int = 2000
     //println(storeWorker)
 
