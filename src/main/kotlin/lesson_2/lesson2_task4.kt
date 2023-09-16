f04962d74ae54084de565187f7aefe1a2378dde7
jguncheg <kelle@list.ru>
2023-09-16 17:34:30 +0300

    KS-2-2 Расходы по ЗП

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+   val crystal = 7
+   val iron = 11
+   val crystalBuff = crystal*20/100
+   val ironBuff = iron*20/100
+
+   println("Crystal Buff - $crystalBuff")
+   println("Iron Buff - $ironBuff")
+
+}
