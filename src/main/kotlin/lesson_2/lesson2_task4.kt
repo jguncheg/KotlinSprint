33ab466e2ca06b4382cb2ae748903678e8a429cd
jguncheg <kelle@list.ru>
2023-09-17 00:22:23 +0300

    KS-2-4 вынес процент в отдельную переменную

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,9 +1,10 @@
 fun main(){
 
-   val crystal = 7
+   val crystal  = 7
    val iron = 11
-   val crystalBuff = crystal*20/100
-   val ironBuff = iron*20/100
+   val percent = 20
+   val crystalBuff = crystal * percent / 100
+   val ironBuff = iron * percent / 100
 
    println("Crystal Buff - $crystalBuff")
    println("Iron Buff - $ironBuff")
