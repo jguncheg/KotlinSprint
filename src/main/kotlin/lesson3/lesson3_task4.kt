00b5092d9f47c66d606f31bf034472019e9ade6d
jguncheg <kelle@list.ru>
2023-09-17 23:10:57 +0300

    KS-3-4 шахматы

---
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    var move = "E2"
+    var moveTo = "E4"
+    var moveNumber = "ход №1"
+
+    println("$move > $moveTo $moveNumber")
+
+    moveNumber = "ход №2"
+    move = "D2"
+    moveTo = "D3"
+
+    println("$move > $moveTo $moveNumber")
+
+}
