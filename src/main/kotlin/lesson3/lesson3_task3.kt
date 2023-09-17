86899987c927de8bfa121f7bbab588f2b4b7cbb6
jguncheg <kelle@list.ru>
2023-09-17 18:00:52 +0300

    KS-3-3 калькулятор

---
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -0,0 +1,26 @@
+package lesson3
+
+fun main() {
+    val number = 6
+    val x1 = number * 1
+    val x2 = number * 2
+    val x3 = number * 3
+    val x4 = number * 4
+    val x5 = number * 5
+    val x6 = number * 6
+    val x7 = number * 7
+    val x8 = number * 8
+    val x9 = number * 9
+
+    println("""|$number x 1 = $x1
+               |$number x 2 = $x2
+               |$number x 3 = $x3
+               |$number x 4 = $x4
+               |$number x 5 = $x5
+               |$number x 6 = $x6
+               |$number x 7 = $x7
+               |$number x 8 = $x8
+               |$number x 9 = $x9""".trimMargin())
+
+
+}
