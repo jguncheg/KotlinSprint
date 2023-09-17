ce3a77b352f08fca394940e390fc72b23e1c61cc
jguncheg <kelle@list.ru>
2023-09-18 02:28:13 +0300

    KS-4-1 доступность столиков

---
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    val today = 13
+    val tomorrow = 9
+    val compare:Boolean =  allPlaces > today
+    val compare2:Boolean = tomorrow < allPlaces
+
+ println("Доступность столиков на сегодня: $compare \nДоступность столиков на завтра: $compare2")
+
+
+}
+    const val allPlaces = 13
