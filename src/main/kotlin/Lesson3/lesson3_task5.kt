69fab5d52922654b104f2f11b18cf0e0d1744756
jguncheg <kelle@list.ru>
2023-09-18 16:45:55 +0300

    KS-3-5  автоформатирование

--- src/main/kotlin/Lesson3/lesson3_task5.kt
+++ src/main/kotlin/Lesson3/lesson3_task5.kt
@@ -1,10 +1,10 @@
 fun main() {
     val input = "D2-D4;0"
 
-    val split = input.split("-",";")
+    val split = input.split("-", ";")
     val from = split[0]
-    val to = split [1]
-    val move = split [2]
+    val to = split[1]
+    val move = split[2]
     println("Откуда: $from")
     println("Куда: $to")
     println("Номер хода: $move")
