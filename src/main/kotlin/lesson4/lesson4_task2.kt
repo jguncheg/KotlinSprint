c629285972394b078e0d637693c048d328501dab
jguncheg <kelle@list.ru>
2023-09-23 20:39:37 +0300

    KS-4-2 добавил переменные пофиксил максимальный обьем

--- src/main/kotlin/lesson4/lesson4_task2.kt
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -1,10 +1,13 @@
-
-fun main(){
+fun main() {
     val kg = 20
     val liters = 80
     val kg2 = 50
     val liters2 = 100
+    val minKg = 30
+    val maxKG = 100
 
-    println("Груз весом $kg и объемом $liters соответствует категории 'Average': ${(kg >= 30 && kg<= 100) and (liters < 100)}\n" +
-            "Груз весом $kg2 и объемом $liters2 соответствует категории 'Average': ${(kg2 >= 30 && kg2<= 100) and (liters2 <= 100)}")
+    println(
+        "Груз весом $kg и объемом $liters соответствует категории 'Average': ${(kg >= minKg && kg <= maxKG) and (liters < 100)}\n" +
+                "Груз весом $kg2 и объемом $liters2 соответствует категории 'Average': ${(kg2 >= minKg && kg2 <= maxKG) and (liters2 < 100)}"
+    )
 }
