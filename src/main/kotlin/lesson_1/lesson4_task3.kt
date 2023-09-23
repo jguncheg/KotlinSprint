aef7de3d8d8bd6ca8095c645432c0fbd8888c6fa
jguncheg <kelle@list.ru>
2023-09-23 22:07:23 +0300

    KS-4-3  Благоприятные условия  для роста бобовых

--- src/main/kotlin/lesson_1/lesson4_task3.kt
+++ src/main/kotlin/lesson_1/lesson4_task3.kt
@@ -8,4 +8,5 @@ fun main() {
     val conclusion = sunnyWheater == true && openTent == true && airWet == 20 && season != "winter"
 
     println("Благоприятные ли условия сейчас для роста бобовых? :$conclusion" )
+
 }
